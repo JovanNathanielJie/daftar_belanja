1d5780f7dbafa31f8c4789cf81a978c0540b2caa
JovanNathanielJie <jovannathanieljie_2327240013@mhs.mdp.ac.id>
2026-04-08 14:39:04 +0700

    Update shopping_list_screen.dart

--- lib/screens/shopping_list_screen.dart
+++ lib/screens/shopping_list_screen.dart
@@ -1,5 +1,4 @@
 import "package:flutter/material.dart";
-import "package:firebase_database/firebase_database.dart";
 import "package:daftar_belanja/services/shopping_service.dart";
 
 class ShoppingListScreen extends StatefulWidget {
